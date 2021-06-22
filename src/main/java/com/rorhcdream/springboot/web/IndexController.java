e103417909cbbfd2250d6f334853a642180e0530
Jaewoong Kim <rorhcdream@gmail.com>
2021-06-22 15:49:57 +0900

    Add post save feature

--- src/main/java/com/rorhcdream/springboot/web/IndexController.java
+++ src/main/java/com/rorhcdream/springboot/web/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/post/save")
+    public String postSave() {
+        return "post-save";
+    }
 }
