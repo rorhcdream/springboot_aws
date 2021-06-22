c5c948206c25bdd0b4b977fca9b98d5669854c74
Jaewoong Kim <rorhcdream@gmail.com>
2021-06-22 17:07:36 +0900

    Add update post feature

--- src/main/java/com/rorhcdream/springboot/web/IndexController.java
+++ src/main/java/com/rorhcdream/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.rorhcdream.springboot.web;
 
 import com.rorhcdream.springboot.service.post.PostService;
+import com.rorhcdream.springboot.web.dto.post.PostResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/post/save")
     public String postSave() {
         return "post-save";
+    }
+
+    @GetMapping("/post/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model) {
+        PostResponseDto dto = postService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "post-update";
     }
 }
