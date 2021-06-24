f679d5ad088e16d1a1fc04f34fcba79e8b6a400b
Jaewoong Kim <rorhcdream@gmail.com>
2021-06-24 23:39:30 +0900

    Add naver login

--- src/main/java/com/rorhcdream/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/rorhcdream/springboot/config/auth/dto/OAuthAttributes.java
@@ -17,6 +17,9 @@ public class OAuthAttributes {
     private String picture;
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if (registrationId.equals("naver")) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -26,6 +29,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
